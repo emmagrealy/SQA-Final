8d2dbdab2f860dd7b30f897078681ddc6ca3ecde
emmagrealy <44479033+emmagrealy@users.noreply.github.com>
2020-05-12 13:05:49 +0100

    Creation of Questions and Survey classes

---
+++ SQA-Final/src/Questions.java
@@ -0,0 +1,21 @@
+public class Questions {
+
+	//question
+	private String question;
+
+	//constructor
+	public Questions(String question) {
+		this.question = question;
+	}
+	
+	//Getters and setters
+	public String getQuestions() {
+		return question;
+	}
+
+	public void setQuestions(String question) {
+		this.question = question;
+	}
+
+	
+}
---
+++ SQA-Final/src/Survey.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+public class Survey {
+	
+	//Survey
+	private String name;
+	
+	//Collection 
+	private ArrayList<Questions> questions = new ArrayList<Questions>();
+
+	//Constructor 
+	public Survey(String name, ArrayList<Questions> questions) {
+		this.name = name;
+		this.questions = questions;
+	}
+	
+	//public void
+	public void addQuestion(Questions question)
+	{
+		this.questions.add(question);
+	}
+	
+	
+	//Getters and setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Questions> getQuestions() {
+		return questions;
+	}
+	public void setQuestions(ArrayList<Questions> questions) {
+		this.questions = questions;
+	}
+
+}
